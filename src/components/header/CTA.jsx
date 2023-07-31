ee2747a6cbb4de9a9703555f7352bcecb37b60ea
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-08-01 00:12:51 +0300

    header component minute 1 hour and 7 minutes

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+export const CTA = () => {
+  return (
+    <div className="cta">
+      <a href="" className="btn">
+        download cv
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        lets tack
+      </a>
+    </div>
+  );
+};
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,26 @@
 import "./header.css";
+import { CTA } from "./CTA";
+import ME from "../../assets/header.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>header </div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>hello iam</h5>
+        <h1>ashraf sayed</h1>
+        <h5 className="text-light">fullstack web developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+        <a href="#contact" className="scroll_down">
+          scroll down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
